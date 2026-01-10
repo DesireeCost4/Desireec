8286e948bf5d5155d50a85f1700881369cd3c6b7
DesireeCosta4 <desireec.dev@gmail.com>
2026-01-10 20:12:58 -0300

    att de css/animações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my_selfie';
+
+  ngOnInit() {
+   
+    AOS.init({
+      duration: 1000, 
+      once: true,    
+    });
+  }
+
 }
